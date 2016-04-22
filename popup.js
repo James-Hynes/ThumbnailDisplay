877fffc7fee22bd8b4ed3b56dabaec07bdd13770
= <james.hynes@sonomaacademy.org>
2016-04-21 20:03:27 -0700

    Added links to image

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   var img = document.getElementById('thumbnail');
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
-    img.src = `https://i.ytimg.com/vi/${tabs[0].url.split('https://www.youtube.com/watch?v=')[1]}/maxresdefault.jpg`;
+    var thumb_url = tabs[0].url.split('https://www.youtube.com/watch?v=')[1];
+    img.src = `https://i.ytimg.com/vi/${thumb_url}/hqdefault.jpg`;
+    document.getElementById('thumbnail_link').href = `https://i.ytimg.com/vi/${thumb_url}/maxresdefault.jpg`;;
   });
+
 });
