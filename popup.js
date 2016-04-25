cf0aac0e3cf274cd1591cb908e71f4fd916b518f
= <james.hynes@sonomaacademy.org>
2016-04-24 21:00:27 -0700

    Small bug fixes

--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   var img = document.getElementById('thumbnail');
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
-    var thumb_url = tabs[0].url.split('https://www.youtube.com/watch?v=')[1];
-    img.src = `https://i.ytimg.com/vi/${thumb_url}/hqdefault.jpg`;
-    document.getElementById('thumbnail_link').href = `https://i.ytimg.com/vi/${thumb_url}/maxresdefault.jpg`;;
+    var url = tabs[0].url;
+    if(url.includes('https://www.youtube.com/watch?v=')) {
+      var thumb_url = tabs[0].url.split('https://www.youtube.com/watch?v=')[1];
+      img.src = `https://i.ytimg.com/vi/${thumb_url}/hqdefault.jpg`;
+      document.getElementById('thumbnail_link').href = `https://i.ytimg.com/vi/${thumb_url}/maxresdefault.jpg`;
+    } else {
+      var textEntry = document.createElement('INPUT');
+      textEntry.setAttribute('autofocus', true);
+    }
   });
-
 });
