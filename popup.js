8ea7d0d2c34aa329dcacd30e4d79d2129e119df1
James R. Hynes <james.hynes@sonomaacademy.org>
2016-04-26 08:32:33 -0700

    Improved URL Input

--- popup.js
+++ popup.js
@@ -1,15 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var img = document.getElementById('thumbnail');
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
     var url = tabs[0].url;
     if(url.includes('https://www.youtube.com/watch?v=')) {
-      var thumb_url = parseURL(url);
-      img.src = `https://i.ytimg.com/vi/${thumb_url}/hqdefault.jpg`;
-      document.getElementById('thumbnail_link').href = `https://i.ytimg.com/vi/${thumb_url}/maxresdefault.jpg`;
+      loadImage(url)
+      console.log('2');
+    } else {
+      console.log('1');
+      textEntry();
     }
   });
 });
 
 function parseURL(url) {
   return (url.match(/watch\?v=[^\&]+/)[0].replace('watch?v=', ''));
+}
+
+function loadImage(url) {
+  var img = document.getElementById('thumbnail');
+  var thumb_id = parseURL(url);
+  img.src = `https://i.ytimg.com/vi/${thumb_id}/hqdefault.jpg`;
+  document.getElementById('thumbnail_link').href = `https://i.ytimg.com/vi/${thumb_id}/maxresdefault.jpg`;
+}
+
+function textEntry() {
+  var input = document.createElement('INPUT');
+  input.id = 'text-box';
+  document.body.appendChild(input);
+  input.addEventListener('keypress', function(info) {
+    if(info['keyIdentifier'] === 'Enter') {
+      loadImage(input.value);
+      document.body.removeChild(document.getElementById('text-box'));
+    }
+  });
 }
